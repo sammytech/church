8a39d25165f893dc49277a03a0846dd1cce06b4d
Samuel Babalola <sammytech.net@gmail.com>
2016-08-04 13:50:44 -0400

    Updates

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     });
 
 
+
       $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
           $(".navbar-fixed-top").removeClass("deactivateBg")
